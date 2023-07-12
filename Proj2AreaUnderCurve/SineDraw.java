cb50f64cdbe3105a7adc731e2ceed094d00c4791
fadedloon <127723191+fadedloon@users.noreply.github.com>
2023-07-11 18:18:48 -0700

    Add files via upload
    Project 2 - Area Under the Curve

---
+++ Proj2AreaUnderCurve/SineDraw.java
@@ -0,0 +1,8 @@
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class SineDraw {
+    public static void drawSineWave() {
+        System.out.print("blah");
+    }
+}
---
+++ Proj2AreaUnderCurve/SineUI.java
@@ -0,0 +1,5 @@
+public class SineUI {
+    public static void main(String[] args) {
+        SineDraw.
+    }
+}
