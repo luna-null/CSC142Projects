192c535fe97364044b3974f89de55571283c28f9
fadedloon <127723191+fadedloon@users.noreply.github.com>
2023-07-12 11:37:04 -0700

    Update SineDraw.java

--- Proj2AreaUnderCurve/SineDraw.java
+++ Proj2AreaUnderCurve/SineDraw.java
@@ -2,7 +2,21 @@ import java.awt.Graphics;
 import java.awt.Color;
 
 public class SineDraw {
-    public static void drawSineWave() {
-        System.out.print("blah");
+    public static void main(String[] args) {
+        DrawingPanel graph = new DrawingPanel();
+        Graphics graphics = graph.getGraphics();
+        graphics.setColor(Color.RED);
+        drawSineWave(graphics);
+    }
+    public static void drawSineWave(Graphics graphic) {
+        double h = Math.PI/32;
+        for (double theta = 0; theta <= Math.PI/2; theta += h) {
+            graphic.drawLine(
+            (int) (theta),
+            (int) (Math.sin(theta)),
+            (int) (theta + h),
+            (int) (Math.sin(theta + h))
+            );
+        }
     }
 }
