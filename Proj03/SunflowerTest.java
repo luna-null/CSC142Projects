863a927622312ad72b349542ff223dcdc132cc5d
fadedloon <127723191+fadedloon@users.noreply.github.com>
2023-07-25 13:19:23 -0700

    please work

--- Proj03/SunflowerTest.java
+++ Proj03/SunflowerTest.java
@@ -1,18 +1,23 @@
-package Proj03.tests;
+package Proj03;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
 import org.junit.*;
 
-import Proj03.src.Sunflower;
+import Proj03.src.*;
 
 
 public class SunflowerTest {
+    
+    @Before
+    public void initEach(){
+        Sunflower testSunflower = new Sunflower(100, 50, 4);
+
+}
 
     @Test
     public void sunTest {
-        Sunflower testSunflower = new Sunflower(100, 50, 4);
+        
         assertEquals(null, 0, 0, 0);
     }
 }
